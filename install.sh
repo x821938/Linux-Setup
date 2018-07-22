0a6214db62ae5547f13ef234a5fd6f14af7b9bb8
Alex Jensen <x821938@offline.dk>
2018-07-22 16:25:34 +0200

    source dir of script

--- install.sh
+++ install.sh
@@ -12,7 +12,8 @@ rm -rf ~/.oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Install my configfiles from vim, tmux and zsh
-cp -R home/. ~
+srcdir=`dirname $0`
+cp -R $srcdir/home/. ~
 
 # Install oh-my-zsh syntax highlight plugin
 git clone https://github.com/zsh-users/zsh-syntax-highlighting/ ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
